14a501a29eaf555eebb7844aa65a51abd1780ecc
ilya <i.titaev@ringme.ru>
2023-04-04 20:25:06 +0300

    add redis not tested

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 """Main entrypoint for the app."""
 import logging
 import pickle
+import json
+from operator import itemgetter
 from pathlib import Path
 from typing import Optional
 
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import PlainTextResponse
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.staticfiles import StaticFiles
 from langchain.vectorstores import VectorStore
 
 from callback import QuestionGenCallbackHandler, StreamingLLMCallbackHandler
 from query_data import get_chain
 from schemas import ChatResponse
+from usersData import getUsersData
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 vectorstore: Optional[VectorStore] = None
 
-
-@app.on_event("startup")
-async def startup_event():
-    logging.info("loading vectorstore")
-    if not Path("vectorstore.pkl").exists():
-        raise ValueError("vectorstore.pkl does not exist, please run ingest.py first")
-    with open("vectorstore.pkl", "rb") as f:
-        global vectorstore
-        vectorstore = pickle.load(f)
+# app.add_middleware(HTTPSRedirectMiddleware)
+# app.mount("/.well-known/pki-validation", StaticFiles(directory="./.well-known/pki-validation"), name="static")
 
 
 @app.get("/")
 async def get(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+         return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/", response_class=PlainTextResponse)
+async def get():
+        return "Hello World"
 
 
 @app.websocket("/chat")
@@ -38,15 +42,23 @@ async def websocket_endpoint(websocket: WebSocket):
     question_handler = QuestionGenCallbackHandler(websocket)
     stream_handler = StreamingLLMCallbackHandler(websocket)
     chat_history = []
-    qa_chain = get_chain(vectorstore, question_handler, stream_handler)
-    # Use the below line instead of the above line to enable tracing
-    # Ensure `langchain-server` is running
-    # qa_chain = get_chain(vectorstore, question_handler, stream_handler, tracing=True)
+    users_data = getUsersData()
 
     while True:
         try:
             # Receive and send back the client message
-            question = await websocket.receive_text()
+            request = await websocket.receive_text()
+            question, clientId, persistHistory = itemgetter('question', 'clientId', 'persistHistory')(json.loads(request))
+            user = users_data.get(clientId)
+            if not(user):
+                resp = ChatResponse(
+                    sender="bot",
+                    message="Client with such id doesn't exist.",
+                    type="error",
+                )
+                await websocket.send_json(resp.dict())
+                continue
+            clientVector = VectorStore()
             resp = ChatResponse(sender="you", message=question, type="stream")
             await websocket.send_json(resp.dict())
 
@@ -54,10 +66,14 @@ async def websocket_endpoint(websocket: WebSocket):
             start_resp = ChatResponse(sender="bot", message="", type="start")
             await websocket.send_json(start_resp.dict())
 
+            lim_chat_history = chat_history[-5:] if persistHistory else []
+
+            qa_chain = get_chain(clientVector, question_handler, stream_handler, user['condense_template'], user['template'], custom_results="Stability Balls (Swiss Balls) • Balance products; Bosu Ball, Wobble boards and balance boards • Dumbbells • Roman Chair A stability ball is not only inexpensive, easy to use and readily available but it also improves balance, coordination and strengthens those hard to get to muscles. To ensure that the correct size stability ball is used, sit on top of the ball with the feet hip width apart. The knees should be level with the hips, and a 90-degree angle should be formed at the knee joint between the legs and thighs. Based on height, the following stability ball sizes are recommended: • 4’ 11”- 5’ 3” you should be using a 55cm ball • 5’ 4”-5’ 10” you should be using a 65cm ball • 5’11” and up you should be using a 75 cm ball. 99 Buy Now Costco Pickleball Paddle Set (Purchase Online Or In Person) Bundle Includes- 2 Latitude Paddles, 3 Balls, and 1 Bag.... Shop • Paddles • Gear • Brands • Programs • Watch • Resources • Help • Blog Information • Contact us • Start a warranty claim • Refund Policy • Shipping Policy • FAQ • Terms of Service • Privacy Policy • Trademarks • Sitemap All paddles and equipment are only intended to be used in Pickleball play with a Pickleball ball. They are not intended to be used as toys or by infants and children. Close Customer Login If you are already registered, please log in. Shipping Taxes and shipping fee will be calculated at checkout  Source page: https://www.selkirk. add as I did a ball mesh bag hold a few extra balls or other items so I was not digging around for them. 3) include a fence hook 4) color the inside of the bag in a bright color to easily see/find items in bag. This may not be as bad with the blue bag. It works, and there was no misleading of info in the description and I do like it, could be better than a normal backpack though. Shop • Paddles • Gear • Brands • Programs • Watch • Resources • Help • Blog Information • Contact us • Start a warranty claim • Refund Policy • Shipping Policy • FAQ • Terms of Service • Privacy Policy • Trademarks • Sitemap")
+
             result = await qa_chain.acall(
-                {"question": question, "chat_history": chat_history}
+               {"question": question, "chat_history": lim_chat_history}
             )
-            chat_history.append((question, result["answer"]))
+            chat_history.append((result['question'], result["answer"]))
 
             end_resp = ChatResponse(sender="bot", message="", type="end")
             await websocket.send_json(end_resp.dict())
--- query_data.py
+++ query_data.py
@@ -2,16 +2,63 @@
 from langchain.callbacks.base import AsyncCallbackManager
 from langchain.callbacks.tracers import LangChainTracer
 from langchain.chains import ChatVectorDBChain
-from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT,
-                                                     QA_PROMPT)
 from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain.llms import OpenAIChat
 from langchain.vectorstores.base import VectorStore
+from langchain.prompts.prompt import PromptTemplate
+from typing import Any, Dict, List, Tuple, Optional
+from langchain.docstore.document import Document
+
+
+def _get_chat_history(chat_history: List[Tuple[str, str]]) -> str:
+    buffer = ""
+    for human_s, ai_s in chat_history:
+        human = "Human: " + human_s
+        ai = "Assistant: " + ai_s
+        buffer += "\n" + "\n".join([human, ai])
+    return buffer
+
+
+class MyChatVectorDBChain(ChatVectorDBChain):
+    custom_results: Optional[str] = None
+
+    def __init__(self, custom_results, **kwargs):
+        super().__init__(**kwargs)  # вызываем конструктор родительского класса
+        self.custom_results = custom_results
+
+    async def _acall(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+        question = inputs["question"]
+        chat_history_str = _get_chat_history(inputs["chat_history"])
+        vectordbkwargs = inputs.get("vectordbkwargs", {})
+        if chat_history_str:
+            new_question = await self.question_generator.arun(
+                question=question, chat_history=chat_history_str
+            )
+        else:
+            new_question = question
+        # TODO: This blocks the event loop, but it's not clear how to avoid it.
+        if self.custom_results:
+            doc = Document(
+                page_content=self.custom_results
+            )
+            docs = [doc]
+        else:
+            docs = self.vectorstore.similarity_search(
+                new_question, k=self.top_k_docs_for_context, **vectordbkwargs
+            )
+        new_inputs = inputs.copy()
+        new_inputs["question"] = new_question
+        new_inputs["chat_history"] = chat_history_str
+        answer, _ = await self.combine_docs_chain.acombine_docs(docs, **new_inputs)
+        if self.return_source_documents:
+            return {self.output_key: answer, "source_documents": docs}
+        else:
+            return {self.output_key: answer}
 
 
 def get_chain(
-    vectorstore: VectorStore, question_handler, stream_handler, tracing: bool = False
+    vectorstore: VectorStore, question_handler, stream_handler, condense_template, qa_prompt, tracing: bool = False, custom_results=None
 ) -> ChatVectorDBChain:
     """Create a ChatVectorDBChain for question/answering."""
     # Construct a ChatVectorDBChain with a streaming llm for combine docs
@@ -26,18 +73,14 @@ def get_chain(
         question_manager.add_handler(tracer)
         stream_manager.add_handler(tracer)
 
-    question_gen_llm = OpenAI(
+    question_gen_llm = OpenAIChat(
         temperature=0,
         verbose=True,
         callback_manager=question_manager,
     )
-    streaming_llm = OpenAI(
-        streaming=True,
-        callback_manager=stream_manager,
-        verbose=True,
-        temperature=0,
-    )
-
+    streaming_llm = OpenAIChat(streaming=True, callback_manager=stream_manager, verbose=True, temperature=0, max_tokens=675)
+    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_template)
+    QA_PROMPT = PromptTemplate(template=qa_prompt, input_variables=["question", "context"])
     question_generator = LLMChain(
         llm=question_gen_llm, prompt=CONDENSE_QUESTION_PROMPT, callback_manager=manager
     )
@@ -45,10 +88,12 @@ def get_chain(
         streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
     )
 
-    qa = ChatVectorDBChain(
+    qa = MyChatVectorDBChain(
+        custom_results,
         vectorstore=vectorstore,
         combine_docs_chain=doc_chain,
         question_generator=question_generator,
         callback_manager=manager,
+        top_k_docs_for_context=4,
     )
     return qa
